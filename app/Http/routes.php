8012793c3a82eaad90f65b396a48c6d7181b2d60
江肖 <407505475@qq.com>
2017-04-09 12:47:41 +0800

     新增前台页面路由

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,12 +11,59 @@
 |
 */
 
+// 首页
 Route::get('/', function () {
-    return view('welcome');
+    return view('mail.home.index');
 });
 
-Route::get('/users', 'UserController@index');
+// 账户中心
+Route::group(['prefix' => 'account'], function() {
+    // 登录
+    Route::get('login', function() {
+        return view('mall.account.login');
+    });
 
-Route::get('/admin', function() {
-    return view('admin.index');
+    // 注册
+    Route::get('register', function() {
+        return view('mail.account.register');
+    });
+});
+
+// 商品
+Route::group(['prefix' => 'goods'], function() {
+    // 商品详情
+    Route::get('detail', function() {
+        return view('mail.goods.detail');
+    });
+
+    // 商品列表
+    Route::get('list', function() {
+        return view('mail.goods.list');
+    });
+});
+
+// 个人中心
+Route::group(['prefix' => 'mine'], function() {
+    // 修改个人资料
+    Route::get('change', function() {
+        return view('mail.mine.change');
+    });
+
+    // 个人信息
+    Route::get('info', function() {
+        return view('mail.mine.info');
+    });
+
+    // 发布的商品
+    Route::get('goods', function() {
+        return view('mail.mine.goods');
+    });
+});
+
+// 后台
+Route::group(['prefix' => 'admin'], function() {
+    // 首页
+    Route::get('', function() {
+        return view('admin.index');
+    });
 });
