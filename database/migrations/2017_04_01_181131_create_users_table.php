9fb2d8d34faf03ba14ef5654986b549e6f9516cd
江肖 <407505475@qq.com>
2017-04-09 11:24:59 +0800

    初始化项目

---
+++ database/migrations/2017_04_01_181131_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('account', 16)->unique();
+
+            $table->string('password', 16);
+
+            $table->string('nickname', 16);
+
+            $table->string('avatar');
+
+            $table->string('telephone', 11);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
