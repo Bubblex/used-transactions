cd8ced484908ca3184362aed305a3ef102eb6b4d
江肖 <407505475@qq.com>
2017-04-09 21:57:23 +0800

    登录 注册页

--- resources/views/mall/account/login.blade.php
+++ resources/views/mall/account/login.blade.php
@@ -1,1 +1,42 @@
-<!-- 登录页-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>校园二手网</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<link href="/resource/style/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="/resource/style/login.css" rel="stylesheet" type="text/css" media="all" />
+<script type="text/javascript" src="/resource/vendor/jquery.min.js"></script>
+
+<!--头部logo-->
+<div class="header_bg">
+	<div class="wrap">
+		<div class="header">
+			<div class="logo">
+				<a href="/"><img src="/resource/image/logo.png" alt=""/> </a>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="login-body">
+	<div class="c_login contact-form">
+		<h2>登 录</h2>
+		<div class="input-item">
+			<input type="text" class="textbox" placeholder="请输入账号">
+		</div>
+		<div class="input-item">
+			<input placeholder="请输入密码" type="text" class="textbox">
+		</div>
+		<div>
+			<span>
+				<input type="submit" class="login-btn" value="登录" >
+			</span>
+		</div>
+		<div>
+			<span><input type="submit" class="login-btn" value="注册" onClick="location.href='/account/register';"></span>
+		</div>
+	</div>
+</div>
+</body>
+</html>
--- resources/views/mall/home/index.blade.php
+++ resources/views/mall/home/index.blade.php
@@ -1,57 +1,55 @@
-<!--首页-->
 <!DOCTYPE HTML>
 <html>
 <head>
-<title>Free Aditii Website Template | Home </title>
+<title>校园二手网</title>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
-<link href="css/app.css" rel="stylesheet" type="text/css" media="all" />
-<script type="text/javascript" src="js/jquery.min.js"></script>
+<link href="/resource/style/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="/resource/style/app.css" rel="stylesheet" type="text/css" media="all" />
+<script type="text/javascript" src="/resource/vendor/jquery.min.js"></script>
 <!-- start slider -->
-	<link href="css/slider.css" rel="stylesheet" type="text/css" media="all" />
-	<script type="text/javascript" src="js/modernizr.custom.28468.js"></script>
-	<script type="text/javascript" src="js/jquery.cslider.js"></script>
-	<script type="text/javascript">
-		$(function() {
-			$('#da-slider').cslider();
+<link href="/resource/style/slider.css" rel="stylesheet" type="text/css" media="all" />
+<script type="text/javascript" src="/resource/vendor/modernizr.custom.28468.js"></script>
+<script type="text/javascript" src="/resource/vendor/jquery.cslider.js"></script>
+<script type="text/javascript">
+$(function() {
+	$('#da-slider').cslider();
+});
+</script>
+<!-- Owl Carousel Assets -->
+<link href="/resource/style/owl.carousel.css" rel="stylesheet">
+<!-- Owl Carousel Assets -->
+<!-- Prettify -->
+<script src="/resource/vendor/owl.carousel.js"></script>
+<script>
+	$(document).ready(function() {
+
+	$("#owl-demo").owlCarousel({
+		items : 4,
+		lazyLoad : true,
+		autoPlay : true,
+		navigation : true,
+		navigationText : ["",""],
+		rewindNav : false,
+		scrollPerPage : false,
+		pagination : false,
+		paginationNumbers: false,
 		});
-	</script>
-		<!-- Owl Carousel Assets -->
-		<link href="css/owl.carousel.css" rel="stylesheet">
-		     <!-- Owl Carousel Assets -->
-		    <!-- Prettify -->
-		    <script src="js/owl.carousel.js"></script>
-		        <script>
-		    $(document).ready(function() {
-
-		      $("#owl-demo").owlCarousel({
-		        items : 4,
-		        lazyLoad : true,
-		        autoPlay : true,
-		        navigation : true,
-			    navigationText : ["",""],
-			    rewindNav : false,
-			    scrollPerPage : false,
-			    pagination : false,
-    			paginationNumbers: false,
-		      });
-
-		    });
-		    </script>
-		   <!-- //Owl Carousel Assets -->
-		<!-- start top_js_button -->
-		<script type="text/javascript" src="js/move-top.js"></script>
-		<script type="text/javascript" src="js/easing.js"></script>
-		   <script type="text/javascript">
-				jQuery(document).ready(function($) {
-					$(".scroll").click(function(event){
-						event.preventDefault();
-						$('html,body').animate({scrollTop:$(this.hash).offset().top},1200);
-					});
-				});
-			</script>
+	});
+</script>
+<!-- //Owl Carousel Assets -->
+<!-- start top_js_button -->
+<script type="text/javascript" src="/resource/vendor/move-top.js"></script>
+<script type="text/javascript" src="/resource/vendor/easing.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1200);
+		});
+	});
+</script>
 </head>
 <body>
 <!-- BEGIN header -->
@@ -59,7 +57,7 @@
 	<div class="wrap">
 		<div class="header">
 			<div class="logo">
-				<a href="index.html"><img src="images/logo.png" alt=""/> </a>
+				<a href="/"><img src="/resource/image/logo.png" alt=""/> </a>
 			</div>
 
 			<div class="h_search">
@@ -71,16 +69,15 @@
 
 			<div class="h_login">
 				<a class="add_a" href="#">注册</a>
-				<a class="add_a" href="#">登录</a>
+				<a class="add_a" href="/account/login">登录</a>
 			</div>
 
 			<div class="logined">
 				<a href="javascript:">
-					<img src="images/logo.png">
+					<img src="/resource/image/logo.png">
 					<span>昵称啊啊啊</span>
 				</a>
 			</div>
-
 		</div>
 	</div>
 </div>
@@ -92,15 +89,17 @@
 		<div class="header_sub">
 			<div class="h_menu">
 				<ul>
-					<li class="active"><a href="index.html">主页</a></li> |
-					<li><a href="handbags.html">书籍</a></li> |
-					<li><a href="accessories.html">闲置数码</a></li> |
-					<li><a href="wallets.html">美妆</a></li> |
-					<li><a href="sale.html">数码</a></li> |
-					<li><a href="index.html">鞋服配饰</a></li> |
-					<li><a href="shoes.html">家居日用</a></li> |
-					<li><a href="service.html">关于我们</a></li> |
-+				</ul>
+					<li class="active"><a href="/">主页</a></li> |
+					<li><a href="/goods/list">校园代步</a></li> |
+					<li><a href="/goods/list">衣物伞帽</a></li> |
+					<li><a href="/goods/list">图书教材</a></li> |
+					<li><a href="/goods/list">数码配件</a></li> |
+					<li><a href="/goods/list">数码</a></li> |
+					<li><a href="/goods/list">手机</a></li> |
+					<li><a href="/goods/list">电脑</a></li> |
+					<li><a href="/goods/list">生活娱乐</a></li> |
+					<li><a href="/goods/list">化妆品</a></li> 
+				</ul>
 			</div>
 			<div class="clear"></div>
 		</div>
@@ -109,45 +108,45 @@
 <!-- END 头部导航 -->
 
 <!-- start slider -->
-			<div id="da-slider" class="da-slider">
-				<div class="da-slide">
-					<h2>welcome to aditii</h2>
-					<p>When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane.</p>
-					<a href="details.html" class="da-link">shop now</a>
-					<div class="da-img"><img src="images/slider1.png" alt="image01" /></div>
-				</div>
-				<div class="da-slide">
-					<h2>Easy management</h2>
-					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-					<a href="details.html" class="da-link">shop now</a>
-					<div class="da-img"><img src="images/slider2.png" alt="image01" /></div>
-				</div>
-				<div class="da-slide">
-					<h2>Revolution</h2>
-					<p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
-					<a href="details.html" class="da-link">shop now</a>
-					<div class="da-img"><img src="images/slider3.png" alt="image01" /></div>
-				</div>
-				<div class="da-slide">
-					<h2>Quality Control</h2>
-					<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
-					<a href="details.html" class="da-link">shop now</a>
-					<div class="da-img"><img src="images/slider4.png" alt="image01" /></div>
-				</div>
-				<nav class="da-arrows">
-					<span class="da-arrows-prev"></span>
-					<span class="da-arrows-next"></span>
-				</nav>
-			</div>
-        </div>
-        <div class="copyrights">Collect from <a href="http://www.cssmoban.com/"  title="网站模板">网站模板</a></div>
+	<div id="da-slider" class="da-slider">
+		<div class="da-slide">
+			<h2>welcome to aditii</h2>
+			<p>When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane.</p>
+			<a href="details.html" class="da-link">shop now</a>
+			<div class="da-img"><img src="/resource/image/slider1.png" alt="image01" /></div>
+		</div>
+		<div class="da-slide">
+			<h2>Easy management</h2>
+			<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+			<a href="details.html" class="da-link">shop now</a>
+			<div class="da-img"><img src="/resource/image/slider2.png" alt="image01" /></div>
+		</div>
+		<div class="da-slide">
+			<h2>Revolution</h2>
+			<p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+			<a href="details.html" class="da-link">shop now</a>
+			<div class="da-img"><img src="/resource/image/slider3.png" alt="image01" /></div>
+		</div>
+		<div class="da-slide">
+			<h2>Quality Control</h2>
+			<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
+			<a href="details.html" class="da-link">shop now</a>
+			<div class="da-img"><img src="/resource/image/slider4.png" alt="image01" /></div>
+		</div>
+		<nav class="da-arrows">
+			<span class="da-arrows-prev"></span>
+			<span class="da-arrows-next"></span>
+		</nav>
+	</div>
+</div>
+<div class="copyrights">Collect from <a href="http://www.cssmoban.com/"  title="网站模板">网站模板</a></div>
 <!----start-cursual---->
 <div class="wrap">
 <!----start-img-cursual---->
 	<div id="owl-demo" class="owl-carousel">
 		<div class="item" onClick="location.href='details.html';">
 			<div class="cau_left">
-				<img class="lazyOwl" data-src="images/c1.jpg" alt="Lazy Owl Image">
+				<img class="lazyOwl" data-src="/resource/image/c1.jpg" alt="Lazy Owl Image">
 			</div>
 			<div class="cau_left">
 				<h4><a href="details.html">branded shoes</a></h4>
@@ -156,7 +155,7 @@
 		</div>
 		<div class="item" onClick="location.href='details.html';">
 			<div class="cau_left">
-				<img class="lazyOwl" data-src="images/c2.jpg" alt="Lazy Owl Image">
+				<img class="lazyOwl" data-src="/resource/image/c2.jpg" alt="Lazy Owl Image">
 			</div>
 			<div class="cau_left">
 				<h4><a href="details.html">branded tees</a></h4>
@@ -165,7 +164,7 @@
 		</div>
 		<div class="item" onClick="location.href='details.html';">
 			<div class="cau_left">
-				<img class="lazyOwl" data-src="images/c3.jpg" alt="Lazy Owl Image">
+				<img class="lazyOwl" data-src="/resource/image/c3.jpg" alt="Lazy Owl Image">
 			</div>
 			<div class="cau_left">
 				<h4><a href="details.html">branded jeens</a></h4>
@@ -174,7 +173,7 @@
 		</div>
 		<div class="item" onClick="location.href='details.html';">
 			<div class="cau_left">
-				<img class="lazyOwl" data-src="images/c2.jpg" alt="Lazy Owl Image">
+				<img class="lazyOwl" data-src="/resource/image/c2.jpg" alt="Lazy Owl Image">
 			</div>
 			<div class="cau_left">
 				<h4><a href="details.html">branded tees</a></h4>
@@ -183,7 +182,7 @@
 		</div>
 		<div class="item" onClick="location.href='details.html';">
 			<div class="cau_left">
-				<img class="lazyOwl" data-src="images/c1.jpg" alt="Lazy Owl Image">
+				<img class="lazyOwl" data-src="/resource/image/c1.jpg" alt="Lazy Owl Image">
 			</div>
 			<div class="cau_left">
 				<h4><a href="details.html">branded shoes</a></h4>
@@ -192,7 +191,7 @@
 		</div>
 		<div class="item" onClick="location.href='details.html';">
 			<div class="cau_left">
-				<img class="lazyOwl" data-src="images/c2.jpg" alt="Lazy Owl Image">
+				<img class="lazyOwl" data-src="/resource/image/c2.jpg" alt="Lazy Owl Image">
 			</div>
 			<div class="cau_left">
 				<h4><a href="details.html">branded tees</a></h4>
@@ -201,7 +200,7 @@
 		</div>
 		<div class="item" onClick="location.href='details.html';">
 			<div class="cau_left">
-				<img class="lazyOwl" data-src="images/c3.jpg" alt="Lazy Owl Image">
+				<img class="lazyOwl" data-src="/resource/image/c3.jpg" alt="Lazy Owl Image">
 			</div>
 			<div class="cau_left">
 				<h4><a href="details.html">branded jeens</a></h4>
@@ -211,13 +210,13 @@
 	</div>
 	<!----//End-img-cursual---->
 </div>
-<!-- start main1 -->
+<!-- start 商品分类的标题 -->
 <div class="main_bg1">
-<div class="wrap">
-	<div class="main1">
-		<h2>featured products</h2>
+	<div class="wrap">
+		<div class="main1">
+			<h2>商品分类</h2>
+		</div>
 	</div>
-</div>
 </div>
 <!-- start main -->
 <div class="main_bg">
@@ -227,7 +226,7 @@
 		<div class="grids_of_3">
 			<div class="grid1_of_3">
 				<a href="details.html">
-					<img src="images/pic1.jpg" alt=""/>
+					<img src="/resource/image/pic1.jpg" alt=""/>
 					<h3>branded shoes</h3>
 					<div class="price">
 						<h4>$300<span>indulge</span></h4>
@@ -237,7 +236,7 @@
 			</div>
 			<div class="grid1_of_3">
 				<a href="details.html">
-					<img src="images/pic2.jpg" alt=""/>
+					<img src="/resource/image/pic2.jpg" alt=""/>
 					<h3>branded t-shirts</h3>
 					<div class="price">
 						<h4>$300<span>indulge</span></h4>
@@ -247,7 +246,7 @@
 			</div>
 			<div class="grid1_of_3">
 				<a href="details.html">
-					<img src="images/pic3.jpg" alt=""/>
+					<img src="/resource/image/pic3.jpg" alt=""/>
 					<h3>branded tees</h3>
 					<div class="price">
 						<h4>$300<span>indulge</span></h4>
@@ -260,7 +259,7 @@
 		<div class="grids_of_3">
 			<div class="grid1_of_3">
 				<a href="details.html">
-					<img src="images/pic4.jpg" alt=""/>
+					<img src="/resource/image/pic4.jpg" alt=""/>
 					<h3>branded bags</h3>
 					<div class="price">
 						<h4>$300<span>indulge</span></h4>
@@ -270,7 +269,7 @@
 			</div>
 			<div class="grid1_of_3">
 				<a href="details.html">
-					<img src="images/pic5.jpg" alt=""/>
+					<img src="/resource/image/pic5.jpg" alt=""/>
 					<h3>ems women bag</h3>
 					<div class="price">
 						<h4>$300<span>indulge</span></h4>
@@ -280,7 +279,7 @@
 			</div>
 			<div class="grid1_of_3">
 				<a href="details.html">
-					<img src="images/pic6.jpg" alt=""/>
+					<img src="/resource/image/pic6.jpg" alt=""/>
 					<h3>branded cargos</h3>
 					<div class="price">
 						<h4>$300<span>indulge</span></h4>
@@ -301,9 +300,9 @@
 		<!-- start grids_of_4 -->
 		<div class="grids_of_4">
 			<div class="grid1_of_4">
-				<h4>featured sale</h4>
+				<h4>友情链接</h4>
 				<ul class="f_nav">
-					<li><a href="">alexis Hudson</a></li>
+					<li><a href="http://hainnu.2shoujie.com/">校园二手街</a></li>
 					<li><a href="">american apparel</a></li>
 					<li><a href="">ben sherman</a></li>
 					<li><a href="">big buddha</a></li>
@@ -377,15 +376,14 @@
 
 			});
 		</script>
-		 <a href="#" id="toTop" style="display: block;"><span id="toTopHover" style="opacity: 1;"></span></a>
+		<a href="#" id="toTop" style="display: block;"><span id="toTopHover" style="opacity: 1;"></span></a>
 		<!--end scroll_top_btn -->
 		<div class="copy">
-			<p class="link">Copyright &copy; 2014.Company name All rights reserved.<a target="_blank" href="http://www.cssmoban.com/">&#x7F51;&#x9875;&#x6A21;&#x677F;</a> -  More Templates <a href="http://www.cssmoban.com/" target="_blank" title="模板之家">模板之家</a></p>
+			<p class="link">Copyright &copy; 2017.Company name All rights reserved.<a target="_blank" href="http://www.cssmoban.com/">管理员</a></p>
 		</div>
 		<div class="clear"></div>
 	</div>
 </div>
 </div>
-
 </body>
 </html>
