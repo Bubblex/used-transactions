945ed5a490cb6a3fab649142884736e699488a85
江肖 <407505475@qq.com>
2017-04-10 19:41:31 +0800

    更换登录注册背景图、新增登录注册页JS

--- resources/views/mall/account/login.blade.php
+++ resources/views/mall/account/login.blade.php
@@ -23,12 +23,12 @@
 	<div class="c_login contact-form">
 		<h2>登 录</h2>
 		<div class="input-item">
-			<input type="text" class="textbox" placeholder="请输入账号">
+			<input id="username" type="text" class="textbox" placeholder="请输入账号">
 		</div>
 		<div class="input-item">
-			<input placeholder="请输入密码" type="text" class="textbox">
+			<input id="pwd" placeholder="请输入密码" type="password" class="textbox">
 		</div>
-		<div>
+		<div id="loginbtn">
 			<span>
 				<input type="submit" class="login-btn" value="登录" >
 			</span>
@@ -38,5 +38,50 @@
 		</div>
 	</div>
 </div>
+<script>
+// 点击登录按钮
+$("#loginbtn").on('click',function() {
+	// 获取用户账号 
+    var username = $('#username').val()
+	// 获取用户密码
+    var pwd = $('#pwd').val()
+    console.log(username)
+    console.log(pwd)
+	// 判断用户是否输入账号
+	if(username === '') {
+		// 弹框提醒
+		alert('请输入账号')
+		// 终止代码执行
+		return
+	}
+	// 判断用户是否输入密码
+	else if(pwd === '') {
+		// 弹框提醒
+		alert('请输入密码')
+		// 终止代码执行
+		return
+	}
+
+	// 验证用户输入账号密码后，进行ajax请求
+	// ajax是一种创建交互式网页应用的网页开发技术，用于创建快速动态网页
+	// 可以实现网页异步更新
+	$.ajax({
+		// 请求地址
+		url: "",
+		// 请求方式 POST 和 TYPE 两种类型
+		type: "POST",
+		// 向服务器发出的请求数据
+		data: {
+			username: username,
+			pwd: pwd
+		},
+		// 请求成功后的回调函数
+		success: function(data) {
+			
+
+		}
+	})
+})
+</script>
 </body>
 </html>
--- resources/views/mall/account/register.blade.php
+++ resources/views/mall/account/register.blade.php
@@ -23,18 +23,18 @@
 	<div class="c_login c_register contact-form">
 		<h2>注 册</h2>
 		<div class="input-item">
-			<input type="text" class="textbox" placeholder="请输入账号">
+			<input id="account" type="text" class="textbox" placeholder="请输入账号">
 		</div>
         <div class="input-item">
-			<input placeholder="请输入昵称" type="text" class="textbox">
+			<input id="nickname" placeholder="请输入昵称" type="text" class="textbox">
 		</div>
 		<div class="input-item">
-			<input placeholder="请输入密码" type="text" class="textbox">
+			<input id="pwd" placeholder="请输入密码" type="password" class="textbox">
 		</div>
         <div class="input-item">
-			<input placeholder="请确认密码" type="text" class="textbox">
+			<input id="confrimpwd" placeholder="请再次输入密码" type="password" class="textbox">
 		</div>
-        <div>
+        <div id="registerbtn">
 			<span><input type="submit" class="login-btn" value="注册"></span>
 		</div>
 		<div>
@@ -45,5 +45,46 @@
 		
 	</div>
 </div>
+<script>
+	$('#registerbtn').on('click',function() {
+		var account = $('#account').val()
+		var nickname = $('#nickname').val()
+		var pwd = $('#pwd').val()
+		var confrimpwd = $('#confrimpwd').val()
+		if(account === '') {
+			alert('请输入账号')
+			return
+		}
+		else if(nickname === '') {
+			alert('请输入昵称')
+			return
+		}
+		else if(pwd === '') {
+			alert('请输入密码')
+			return
+		}
+		else if(confrimpwd === '') {
+			alert('请再次输入密码')
+			return
+		}
+		else if(confrimpwd != pwd) {
+			alert('两次输入密码不一致')
+			return
+		}
+		$.ajax({
+			url: "",
+			type: "POST",
+			data: {
+				account: account,
+				nickname: nickname,
+				pwd: pwd,
+				confrimpwd: confrimpwd
+			},
+			success: function(data) {
+			
+		}
+	})
+	})
+</script>
 </body>
 </html>
