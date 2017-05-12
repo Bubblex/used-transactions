bb81680a2519dc335c33c826acf4d03b31baecf1
江肖 <407505475@qq.com>
2017-05-12 21:46:21 +0800

    详情页

--- resources/views/mall/goods/detail.blade.php
+++ resources/views/mall/goods/detail.blade.php
@@ -1,1 +1,419 @@
 <!--商品详情页-->
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>The Aditii Website Template | Details </title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<link href="/resource/style/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="/resource/style/app.css" rel="stylesheet" type="text/css" media="all" />
+
+<!-- start details -->
+<link rel="stylesheet" type="text/css" href="/resource/style/productviewgallery.css" media="all" />
+<script type="text/javascript" src="/resource/vendor/jquery.min.js"></script>
+<script type="text/javascript" src="/resource/vendor/cloud-zoom.1.0.3.min.js"></script>
+<script type="text/javascript" src="/resource/vendor/jquery.fancybox.pack.js"></script>
+<script type="text/javascript" src="/resource/vendor/jquery.fancybox-buttons.js"></script>
+<script type="text/javascript" src="/resource/vendor/jquery.fancybox-thumbs.js"></script>
+<script type="text/javascript" src="/resource/vendor/productviewgallery.js"></script>
+<!-- start top_js_button -->
+<script type="text/javascript" src="/resource/vendor/jquery.min.js"></script>
+<script type="text/javascript" src="/resource/vendor/move-top.js"></script>
+<script type="text/javascript" src="/resource/vendor/easing.js"></script>
+   <script type="text/javascript">
+		jQuery(document).ready(function($) {
+			$(".scroll").click(function(event){
+				event.preventDefault();
+				$('html,body').animate({scrollTop:$(this.hash).offset().top},1200);
+			});
+		});
+	</script>
+</head>
+<body>
+<!-- BEGIN header -->
+<div class="header_bg">
+	<div class="wrap">
+		<div class="header">
+			<div class="logo">
+				<a href="/"><img src="/resource/image/logo.png" alt=""/> </a>
+			</div>
+
+			<div class="h_search">
+	    		<form>
+	    			<input id="searchinput" type="text" placeholder="请输入搜索关键字">
+	    			<span id="searchbtn" type="submit"></span>
+	    		</form>
+			</div>
+
+			<div class="h_login">
+				<a class="add_a" href="/account/register">注册</a>
+				<a class="add_a" href="/account/login">登录</a>
+			</div>
+
+			<div class="logined">
+				<a href="javascript:">
+					<img src="/resource/image/logo.png">
+					<span>昵称啊啊啊</span>
+					<img class="setting" src="/resource/image/setting.png" onClick="location.href='/mine/info'">
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- END header -->
+
+<!-- BEGIN 头部导航 -->
+<div class="header_btm">
+	<div class="wrap">
+		<div class="header_sub">
+			<div class="h_menu">
+				<ul>
+					<li class="active"><a href="/">主页</a></li> |
+					<li><a href="/goods/list">图书教材</a></li> |
+					<li><a href="/goods/list">数码配件</a></li> |
+					<li><a href="/goods/list">化妆品</a></li> | 
+					<li><a href="/goods/list">数码</a></li> |
+					<li><a href="/goods/list">手机</a></li> |
+					<li><a href="/goods/list">电脑</a></li> |
+					<li><a href="/goods/list">生活娱乐</a></li> |
+					<li><a href="/goods/list">校园代步</a></li> |
+					<li><a href="/goods/list">衣物伞帽</a></li>
+				</ul>
+			</div>
+			<div class="clear"></div>
+		</div>
+	</div>
+</div>
+<!-- END 头部导航 -->
+
+
+<!-- start main -->
+<div class="main_bg">
+<div class="wrap">
+	<div class="main">
+	<!-- start content -->
+	<div class="single">
+			<!-- start span1_of_1 -->
+			<div class="left_content">
+			<div class="span1_of_1">
+				<!-- start product_slider -->
+				<div class="product-view">
+				    <div class="product-essential">
+				        <div class="product-img-box">
+				    <div class="more-views" style="float:left;">
+				        <div class="more-views-container">
+				        <ul>
+				            <li>
+				            	<a class="cs-fancybox-thumbs" data-fancybox-group="thumb" style="width:64px;height:85px;" href=""  title="" alt="">
+				            	<img src="" src_main=""  title="" alt="" /></a>
+				            </li>
+				            <li>
+				            	<a class="cs-fancybox-thumbs" data-fancybox-group="thumb" style="width:64px;height:85px;" href=""  title="" alt="">
+				            	<img src="" src_main=""  title="" alt="" /></a>
+				            </li>
+				            <li>
+				            	<a class="cs-fancybox-thumbs" data-fancybox-group="thumb" style="width:64px;height:85px;" href=""  title="" alt="">
+				            	<img src="" src_main=""  title="" alt="" /></a>
+				            </li>
+				            <li>
+				            	<a class="cs-fancybox-thumbs" data-fancybox-group="thumb" style="width:64px;height:85px;" href=""  title="" alt="">
+				            	<img src="" src_main="" title="" alt="" /></a>
+				            </li>
+				            <li>
+				            	<a class="cs-fancybox-thumbs" data-fancybox-group="thumb" style="width:64px;height:85px;" href=""  title="" alt="">
+				            	<img src="" src_main="" title="" alt="" /></a>
+				            </li>
+				          </ul>
+				        </div>
+				    </div>
+				    <div class="product-image">
+				        <a class="cs-fancybox-thumbs cloud-zoom" rel="adjustX:30,adjustY:0,position:'right',tint:'#202020',tintOpacity:0.5,smoothMove:2,showTitle:true,titleOpacity:0.5" data-fancybox-group="thumb" href="/resource/image/0001-2.jpg" title="Women Shorts" alt="Women Shorts">
+				           	<img src="/resource/image/0001-2.jpg" alt="Women Shorts" title="Women Shorts" />
+				        </a>
+				   </div>
+					<script type="text/javascript">
+						var prodGallery = jQblvg.parseJSON('{"prod_1":{"main":{"orig":"/resource/image/0001-2.jpg","main":"/resource/image/large/0001-2.jpg","thumb":"/resource/image/small/0001-2.jpg","label":""},"gallery":{"item_0":{"orig":"/resource/image/0001-2.jpg","main":"/resource/image/large/0001-2.jpg","thumb":"/resource/image/small/0001-2.jpg","label":""},"item_1":{"orig":"/resource/image/0001-1.jpg","main":"/resource/image/large/0001-1.jpg","thumb":"/resource/image/small/0001-1.jpg","label":""},"item_2":{"orig":"/resource/image/0001-5.jpg","main":"/resource/image/large/0001-5.jpg","thumb":"/resource/image/small/0001-5.jpg","label":""},"item_3":{"orig":"/resource/image/0001-3.jpg","main":"/resource/image/large/0001-3.jpg","thumb":"/resource/image/small/0001-3.jpg","label":""},"item_4":{"orig":"/resource/image/0001-4.jpg","main":"/resource/image/large/0001-4.jpg","thumb":"/resource/image/small/0001-4.jpg","label":""}},"type":"simple","video":false}}'),
+						    gallery_elmnt = jQblvg('.product-img-box'),
+						    galleryObj = new Object(),
+						    gallery_conf = new Object();
+						    gallery_conf.moreviewitem = '<a class="cs-fancybox-thumbs" data-fancybox-group="thumb" style="width:64px;height:85px;" href=""  title="" alt=""><img src="" src_main="" width="64" height="85" title="" alt="" /></a>';
+						    gallery_conf.animspeed = 200;
+						jQblvg(document).ready(function() {
+						    galleryObj[1] = new prodViewGalleryForm(prodGallery, 'prod_1', gallery_elmnt, gallery_conf, '.product-image', '.more-views', 'http:');
+						        jQblvg('.product-image .cs-fancybox-thumbs').absoluteClick();
+						    jQblvg('.cs-fancybox-thumbs').fancybox({ prevEffect : 'none',
+						                             nextEffect : 'none',
+						                             closeBtn : true,
+						                             arrows : true,
+						                             nextClick : true,
+						                             helpers: {
+						                               thumbs : {
+						                                   width: 64,
+						                                   height: 85,
+						                                   position: 'bottom'
+						                               }
+						                             }
+						                             });
+						    jQblvg('#wrap').css('z-index', '100');
+						            jQblvg('.more-views-container').elScroll({type: 'vertical', elqty: 4, btn_pos: 'border', scroll_speed: 400 });
+
+						});
+
+						function initGallery(a,b,element) {
+						    galleryObj[a] = new prodViewGalleryForm(prods, b, gallery_elmnt, gallery_conf, '.product-image', '.more-views', '');
+						};
+					</script>
+				</div>
+				</div>
+				</div>
+				<!-- end product_slider -->
+			</div>
+			<!-- start span1_of_1 -->
+			<div class="span1_of_1_des">
+				  <div class="desc1">
+					<h3>男式短袖 </h3>
+					<p>商品简介商品简介商品简介商品简介商品简介商品简介商品简介商品简介商品简介商品简介商品简介商品简介商品简介商品简介商品简介商品简介</p>
+					<h5>￥：399RMB
+              <!-- <a href="#">click for offer</a> -->
+          </h5>
+					<div class="available">
+						<!-- <h4>Available Options :</h4>
+						<ul>
+							<li>Color:
+								<select>
+								<option>Silver</option>
+								<option>Black</option>
+								<option>Dark Black</option>
+								<option>Red</option>
+							</select></li>
+							<li>Size:
+								<select>
+									<option>L</option>
+									<option>XL</option>
+									<option>S</option>
+									<option>M</option>
+								</select>
+							</li>
+							<li>Quality:
+								<select>
+									<option>1</option>
+									<option>2</option>
+									<option>3</option>
+									<option>4</option>
+									<option>5</option>
+								</select>
+							</li>
+						</ul> -->
+						<div class="btn_form">
+							<form>
+								<input type="submit" value="联系卖家" title="" />
+							</form>
+						</div>
+						<span class="span_right"><a href="#">我要发布产品 </a></span>
+						<div class="clear"></div>
+					</div>
+					<div class="share-desc">
+						<div class="share">
+							<h4>分享产品:</h4>
+							<ul class="share_nav">
+								<li><a href="#"><img src="/resource/image/facebook.png" title="facebook"></a></li>
+								<li><a href="#"><img src="/resource/image/twitter.png" title="Twiiter"></a></li>
+								<li><a href="#"><img src="/resource/image/rss.png" title="Rss"></a></li>
+								<li><a href="#"><img src="/resource/image/gpluse.png" title="Google+"></a></li>
+				    		</ul>
+						</div>
+						<div class="clear"></div>
+					</div>
+			   	 </div>
+			   	</div>
+			   	<div class="clear"></div>
+			   	<!-- start tabs -->
+				   	<section class="tabs">
+		            <input id="tab-1" type="radio" name="radio-set" class="tab-selector-1" checked="checked">
+			        <label for="tab-1" class="tab-label-1">产品详情</label>
+
+		            <input id="tab-2" type="radio" name="radio-set" class="tab-selector-2">
+			        <label for="tab-2" class="tab-label-2">产品规格</label>
+
+		            <input id="tab-3" type="radio" name="radio-set" class="tab-selector-3">
+			        <label for="tab-3" class="tab-label-3">使用情况</label>
+
+				    <div class="clear-shadow"></div>
+
+			        <div class="content">
+				        <div class="content-1">
+				        	<p class="para top"><span>LOREM IPSUM</span> There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined </p>
+							<ul>
+								<li>Research</li>
+								<li>Design and Development</li>
+								<li>Porting and Optimization</li>
+								<li>System integration</li>
+								<li>Verification, Validation and Testing</li>
+								<li>Maintenance and Support</li>
+							</ul>
+							<div class="clear"></div>
+						</div>
+				        <div class="content-2">
+							<p class="para"><span>WELCOME </span> Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections </p>
+							<ul class="qua_nav">
+								<li>Multimedia Systems</li>
+								<li>Digital media adapters</li>
+								<li>Set top boxes for HDTV and IPTV Player applications on various Operating Systems and Hardware Platforms</li>
+							</ul>
+						</div>
+				        <div class="content-3">
+				        	<p class="para top"><span>LOREM IPSUM</span> There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined </p>
+							<ul>
+								<li>Research</li>
+								<li>Design and Development</li>
+								<li>Porting and Optimization</li>
+								<li>System integration</li>
+								<li>Verification, Validation and Testing</li>
+								<li>Maintenance and Support</li>
+							</ul>
+							<div class="clear"></div>
+						</div>
+			        </div>
+			        </section>
+		         	<!-- end tabs -->
+			   	</div>
+			   	<!-- start sidebar -->
+			 <div class="left_sidebar">
+					<div class="sellers">
+						<h4>最新闲置</h4>
+						<div class="single-nav">
+			                <ul>
+                                <li><a href="#"> 九成新 显示器</a></li>
+                                <li><a href="#"> 九成新 显示器</a></li>
+                                <li><a href="#"> 九成新 显示器</a></li>
+                                <li><a href="#"> 九成新 显示器</a></li>
+                                <li><a href="#"> 九成新 显示器</a></li>
+                                <li><a href="#"> 九成新 显示器</a></li>
+                                <li><a href="#"> 九成新 显示器</a></li>
+                                <li><a href="#"> 九成新 显示器</a></li>
+                                <li><a href="#"> 九成新 显示器</a></li>
+			                    <li><a href="#"> 九成新 显示器</a></li>
+			                </ul>
+			              </div>
+						  <div class="banner-wrap bottom_banner color_link">
+								<a href="#" class="main_link">
+								<figure><img src="/resource/image/delivery.png" alt=""></figure>
+								<h5><span>跑腿送货服务</span></h5><p>只要998送到家,电联15233486038</p></a>
+						 </div>
+						 <div class="brands">
+							 <h1>猜你喜欢</h1>
+					  		 <div class="field">
+				                 <select class="select1">
+				                   <option>Please Select</option>
+										<option>Lorem ipsum dolor sit amet</option>
+										<option>Lorem ipsum dolor sit amet</option>
+										<option>Lorem ipsum dolor sit amet</option>
+				                  </select>
+				            </div>
+			    		</div>
+					</div>
+				</div>
+					<!-- end sidebar -->
+          	    <div class="clear"></div>
+	       </div>
+	<!-- end content -->
+	</div>
+</div>
+</div>
+<!-- start footer -->
+<div class="footer_bg">
+<div class="wrap">
+	<div class="footer">
+		<!-- start grids_of_4 -->
+		<div class="grids_of_4">
+			<div class="grid1_of_4">
+				<h4>featured sale</h4>
+				<ul class="f_nav">
+					<li><a href="#">alexis Hudson</a></li>
+					<li><a href="#">american apparel</a></li>
+					<li><a href="#">ben sherman</a></li>
+					<li><a href="#">big buddha</a></li>
+					<li><a href="#">channel</a></li>
+					<li><a href="#">christian audigier</a></li>
+					<li><a href="#">coach</a></li>
+					<li><a href="#">cole haan</a></li>
+				</ul>
+			</div>
+			<div class="grid1_of_4">
+				<h4>mens store</h4>
+				<ul class="f_nav">
+					<li><a href="#">alexis Hudson</a></li>
+					<li><a href="#">american apparel</a></li>
+					<li><a href="#">ben sherman</a></li>
+					<li><a href="#">big buddha</a></li>
+					<li><a href="#">channel</a></li>
+					<li><a href="#">christian audigier</a></li>
+					<li><a href="#">coach</a></li>
+					<li><a href="#">cole haan</a></li>
+				</ul>
+			</div>
+			<div class="grid1_of_4">
+				<h4>women store</h4>
+				<ul class="f_nav">
+					<li><a href="#">alexis Hudson</a></li>
+					<li><a href="#">american apparel</a></li>
+					<li><a href="#">ben sherman</a></li>
+					<li><a href="#">big buddha</a></li>
+					<li><a href="#">channel</a></li>
+					<li><a href="#">christian audigier</a></li>
+					<li><a href="#">coach</a></li>
+					<li><a href="#">cole haan</a></li>
+				</ul>
+			</div>
+			<div class="grid1_of_4">
+				<h4>quick links</h4>
+				<ul class="f_nav">
+					<li><a href="#">alexis Hudson</a></li>
+					<li><a href="#">american apparel</a></li>
+					<li><a href="#">ben sherman</a></li>
+					<li><a href="#">big buddha</a></li>
+					<li><a href="#">channel</a></li>
+					<li><a href="#">christian audigier</a></li>
+					<li><a href="#">coach</a></li>
+					<li><a href="#">cole haan</a></li>
+				</ul>
+			</div>
+			<div class="clear"></div>
+		</div>
+	</div>
+</div>
+</div>
+<!-- start footer -->
+<div class="footer_bg1">
+<div class="wrap">
+	<div class="footer">
+		<!-- scroll_top_btn -->
+	    <script type="text/javascript">
+			$(document).ready(function() {
+
+				var defaults = {
+		  			containerID: 'toTop', // fading element id
+					containerHoverID: 'toTopHover', // fading element hover id
+					scrollSpeed: 1200,
+					easingType: 'linear'
+		 		};
+
+
+				$().UItoTop({ easingType: 'easeOutQuart' });
+
+			});
+		</script>
+		 <a href="#" id="toTop" style="display: block;"><span id="toTopHover" style="opacity: 1;"></span></a>
+		<!--end scroll_top_btn -->
+		<div class="copy">
+			<p class="link">Copyright &copy; 2014.Company name All rights reserved.<a target="_blank" href="http://www.cssmoban.com/">&#x7F51;&#x9875;&#x6A21;&#x677F;</a> -  More Templates <a href="http://www.cssmoban.com/" target="_blank" title="模板之家">模板之家</a></p>
+		</div>
+		<div class="clear"></div>
+	</div>
+</div>
+</div>
+
+</body>
+</html>
